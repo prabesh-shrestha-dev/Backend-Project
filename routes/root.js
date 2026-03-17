5cb350680aba3e52861bb876c8467da3094bee96
Prabesh Shrestha <prabeshshrestha0112>
2026-03-17 15:19:10 +0545

    Chapter 9: MVC REST API

--- routes/root.js
+++ routes/root.js
@@ -6,14 +6,4 @@ router.get(['/', '/index.html', '/index'], (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
 });
 
-router.get(['/new-page.html', '/new-page'], (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'views', 'new-page.html'));
-});
-
-// Redirect
-router.get(['/old-page.html', '/old-page'], (req, res) => {
-  // Permanently moved to new page // 301
-  res.redirect(301, '/new-page.html'); // 302 by default
-});
-
 module.exports = router;
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
@@ -12,22 +13,9 @@ const PORT = process.env.PORT || 3500;
 app.use(logger);
 
 // Cross Origin Resource Sharing
-const whitelist = ['https://www.google.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
-const corsOptions = {
-  origin: (origin, callback) => {
-    if (!origin || whitelist.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  optionSuccessStatus: 200
-}
 app.use(cors(corsOptions));
 
-// built-in middleware to handle urlencoded data
-// in other words, form data:
-// 'content-type: application/x-www-form-urlencoded'
+// built-in middleware to handle urlencoded form data
 app.use(express.urlencoded({ extended: false }));
 
 // built-in middleware for json
@@ -35,15 +23,12 @@ app.use(express.json());
 
 // serve static files
 app.use('/', express.static(path.join(__dirname, '/public')));
-app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 // routes 
 app.use('/', require('./routes/root'));
-app.use('/subdir', require('./routes/subdir'));
 app.use('/employees', require('./routes/api/employees'));
 
 app.all('/{*splat}', (req, res) => {
-  // By default express will send 200 since successful sendFile
   res.status(404);
   if (req.accepts('html')) {
     res.sendFile(path.join(__dirname, 'views', '404.html'));
