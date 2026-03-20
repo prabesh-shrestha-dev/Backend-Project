2f7de1653d9e06c8a1fad4d3789c207feae5fdd3
Prabesh Shrestha <prabeshshrestha0112>
2026-03-20 17:10:14 +0545

    Chapter 12: User Roles | Authorization

--- routes/api/employees.js
+++ routes/api/employees.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const { getAllEmployees, createNewEmployee, updateEmployee, deleteEmployee, getEmployee } = require('../../controllers/employeesController');
+const ROLES_LIST = require('../../config/roles_list');
+const verifyRoles = require('../../middleware/verifyRoles');
 
 router.route('/')
   .get(getAllEmployees)
-  .post(createNewEmployee)
-  .put(updateEmployee)
-  .delete(deleteEmployee);
+  .post(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor),createNewEmployee)
+  .put(verifyRoles(ROLES_LIST.Admin, ROLES_LIST.Editor), updateEmployee)
+  .delete(verifyRoles(ROLES_LIST.Admin), deleteEmployee);
 
 router.route('/:id')
   .get(getEmployee);
