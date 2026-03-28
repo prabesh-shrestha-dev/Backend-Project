89aeaed007f87afeccb36144152e5efa5fef1cd7
Prabesh Shrestha <prabeshshrestha0112>
2026-03-28 15:27:01 +0545

    Brush up

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,7 +2,7 @@ const User = require('../model/User');
 
 const getAllUsers = async (req, res) => {
   const users = await User.find().exec();
-  if (!users) return res.status(204).json({ 'message': 'No users found' });
+  if (users.length === 0) return res.status(204).json({ 'message': 'No users found' });
   res.json(users);
 };
 
@@ -11,12 +11,12 @@ const deleteUser = async (req, res) => {
   const deletedUser = await User.findOneAndDelete({
     _id: req.body.id
   }).exec();
-  if (!deletedUser) return res.status(204).json({ 'message': `User ID ${req.body.id} not found.` });
+  if (!deletedUser) return res.status(404).json({ 'message': `User ID ${req.body.id} not found.` });
   res.json(deletedUser);
 };
 
 const getUser = async (req, res) => {
-  if (!req?.params?.id) return res.status(404).json({ 'message': 'No users found' });
+  if (!req?.params?.id) return res.status(400).json({ 'message': 'User ID required.' });
   const user = await User.findOne({
     _id: req.params.id
   }).exec();
